be1b43204cec65326292c7a2d7ef51b0a629bfd9
PalmeseMattia <palmesedavidemattia@gmail.com>
2026-04-02 01:22:42 +0200

    Parents
    Added parent nodes pointers.
    Need to implement DAG and backprop.
    Maybe there is a better way to create the dag in the forward pass. idk at the moment

--- TensorEngine.py
+++ TensorEngine.py
@@ -2,13 +2,14 @@ import numpy as np
 
 class Tensor:
 
-    def __init__(self, data, _backward=()):
+    def __init__(self, data, _backward=(), _parents=()):
         self.data = data
         self._backward = lambda: None
         self._grad = None
+        self._parents = _parents
 
     def __matmul__(self, other):
-        out = Tensor(self.data @ other.data)
+        out = Tensor(self.data @ other.data, _parents=(self, other))
 
         def _backward():
             self._grad = out._grad @ np.transpose(other.data)
@@ -18,7 +19,7 @@ class Tensor:
         return out
 
     def __add__(self, other):
-        out = self.data + other.data
+        out = Tensor(self.data + other.data, _parents=(self, other))
 
         def _backward():
             self._grad = out._grad
@@ -30,7 +31,18 @@ class Tensor:
     def backward(self, allow_fill=False):
         if allow_fill is True:
             self._grad = np.ones_like(self.data)
+        #TODO: Build DAG
+        #TODO: Call backward recursively
         self._backward()
     
     def __repr__(self):
         return f"Tensor:\n{str(self.data)}\nGrad:\n{str(self._grad)}"
+    
+if __name__ == "__main__":
+    a = Tensor(np.array([1,2,3]))
+    b = Tensor(np.array([1,2,3]))
+    c = a @ b
+    print(a.__hash__)
+    print(b.__hash__)
+    for n in c._parents:
+        print(f'Parent of C: {n.__hash__}')
